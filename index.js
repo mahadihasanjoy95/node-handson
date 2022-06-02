16403953d33b54ce4167a935231f9db8fbe7e5db
joy <mahadi.hasan@mobioapp.com>
2022-06-02 10:40:58 +0600

    USER MODULE COMPLETE

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const http = require('http');
+const UsersRouter = require('./lolo/routes.config');
 const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+const express = require('express');
+const app = express();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  // res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  } else {
+    return next();
+  }
 });
+app.use(express.json());
+UsersRouter.routesConfig(app);
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
